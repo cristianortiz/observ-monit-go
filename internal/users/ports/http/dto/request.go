c985297b99f813aca23f6e9b7f7483fea3207a43
Cristian Ortiz Navia <caortizn@ext.falabella.cl>
2025-10-19 17:44:06 -0300

    feat: DTOs for request, response for user module, also mapper functions to converts data between layers, user domain to user Dto

--- internal/users/ports/http/dto/request.go
+++ internal/users/ports/http/dto/request.go
@@ -5,3 +5,40 @@ type CreateUserRequestDto struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8"`
 }
+
+type UpdateUserRequestDto struct {
+	Name  *string `json:"name,omitempty" validate:"omitempty,min=2,max=100"`
+	Email *string `json:"email,omitempty" validate:"omitempty,email"`
+}
+
+type UpdatePasswordRequestDto struct {
+	OldPassword string `json:"old_password" validate:"required"`
+	NewPassword string `json:"new_password" validate:"required,min=8"`
+}
+
+type LoginRequestDto struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type ListUsersQuery struct {
+	Page     int    `query:"page" validate:"min=1"`
+	PageSize int    `query:"page_size" validate:"min=1,max=100"`
+	SortBy   string `query:"sort_by" validate:"omitempty,oneof=name email created_at"`
+	Order    string `query:"order" validate:"omitempty,oneof=asc desc"`
+}
+
+func (q *ListUsersQuery) SetDefaults() {
+	if q.Page == 0 {
+		q.Page = 1
+	}
+	if q.PageSize == 0 {
+		q.PageSize = 20
+	}
+	if q.SortBy == "" {
+		q.SortBy = "created_at"
+	}
+	if q.Order == "" {
+		q.Order = "desc"
+	}
+}
